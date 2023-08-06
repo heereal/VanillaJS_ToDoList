f091ae35d20f3723492a7ba028f933d2125330ea
heeryoung <winterbooks@naver.com>
2023-08-06 15:06:28 +0900

    feat: addTodo, deleteTodo 기능 구현

--- todo.js
+++ todo.js
@@ -6,14 +6,17 @@ let todosArray = [
   },
 ];
 
+// todosArray 배열 렌더링 및 업데이트
 const paintTodos = () => {
   const todoList = document.querySelector(".todo-list");
   todoList.innerHTML = ""; //todoList 요소 안의 HTML 초기화
+  console.log("paintTodos 실행");
 
   // todosArray를 todoList HTML 요소에 그리기
   todosArray.forEach((todo) => {
     const li = document.createElement("li");
     li.classList.add("todo-item");
+    li.id = todo.id;
 
     const content = document.createElement("div");
     content.innerText = todo.content;
@@ -21,9 +24,9 @@ const paintTodos = () => {
     const btns = document.createElement("div");
     btns.classList.add("todo-btns");
 
-    const completeBtn = document.createElement("button");
-    completeBtn.classList.add("complete-Btn");
-    completeBtn.innerHTML = "완료";
+    // const completeBtn = document.createElement("button");
+    // completeBtn.classList.add("complete-Btn");
+    // completeBtn.innerHTML = "완료";
 
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("delete-Btn");
@@ -32,7 +35,7 @@ const paintTodos = () => {
     todoList.appendChild(li);
     li.appendChild(content);
     li.appendChild(btns);
-    btns.appendChild(completeBtn);
+    // btns.appendChild(completeBtn);
     btns.appendChild(deleteBtn);
   });
 };
@@ -46,24 +49,45 @@ const addTodo = () => {
     isDone: false,
   };
   todosArray.push(newTodo);
-  console.log(todosArray);
+  paintTodos();
 };
 
 // 투두리스트 완료하기
-const completeTodo = () => {
-  console.log("complete");
+const completeTodo = (event) => {
+  // 버튼을 클릭한 li 요소 찾기
+  const todoItem = event.target.closest(".todo-item");
+  // li 요소의 id 가져오기
+  const itemId = todoItem.id;
 };
 
 // 투두리스트 삭제하기
-const deleteTodo = () => {
-  console.log("delete");
+const deleteTodo = (event) => {
+  // 버튼을 클릭한 li 요소 찾기
+  const todoItem = event.target.closest(".todo-item");
+  // li 요소의 id 가져오기
+  const itemId = todoItem.id;
+
+  todosArray = todosArray.filter((todo) => todo.id != itemId);
+  paintTodos();
+};
+
+// 페이지 로딩 시에 실행되는 함수
+window.onload = () => {
+  paintTodos();
 };
 
 document.querySelector(".submit-btn").addEventListener("click", addTodo);
 // document.querySelector(".complete-Btn").addEventListener("click", completeTodo);
 // document.querySelector(".delete-Btn").addEventListener("click", deleteTodo);
 
-// 페이지 로딩 시에 실행되는 함수
-window.onload = () => {
-  paintTodos();
-};
+// 이벤트 위임: .todo-list 요소에 이벤트 리스너 추가
+document.querySelector(".todo-list").addEventListener("click", (event) => {
+  // 클릭한 요소가 complete-Btn 클래스를 가지고 있는지 확인
+  if (event.target.classList.contains("complete-Btn")) {
+    completeTodo(event);
+  }
+
+  if (event.target.classList.contains("delete-Btn")) {
+    deleteTodo(event);
+  }
+});
