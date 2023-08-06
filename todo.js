0d97514016d8793d9ef5bb586842bc3ec9dc7600
heeryoung <winterbooks@naver.com>
2023-08-06 14:30:00 +0900

    feat: paintTodos 기능 구현

--- todo.js
+++ todo.js
@@ -1,17 +1,52 @@
-const todoInput = document.querySelector(".todo-input");
-const submitBtn = document.querySelector(".submit-btn");
+let todosArray = [
+  {
+    id: 1,
+    content: "영화보기",
+    isDone: false,
+  },
+];
 
-let todolist = [];
+const paintTodos = () => {
+  const todoList = document.querySelector(".todo-list");
+  todoList.innerHTML = ""; //todoList 요소 안의 HTML 초기화
+
+  // todosArray를 todoList HTML 요소에 그리기
+  todosArray.forEach((todo) => {
+    const li = document.createElement("li");
+    li.classList.add("todo-item");
+
+    const content = document.createElement("div");
+    content.innerText = todo.content;
+
+    const btns = document.createElement("div");
+    btns.classList.add("todo-btns");
+
+    const completeBtn = document.createElement("button");
+    completeBtn.classList.add("complete-Btn");
+    completeBtn.innerHTML = "완료";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-Btn");
+    deleteBtn.innerHTML = "삭제";
+
+    todoList.appendChild(li);
+    li.appendChild(content);
+    li.appendChild(btns);
+    btns.appendChild(completeBtn);
+    btns.appendChild(deleteBtn);
+  });
+};
 
 // 투두리스트 추가하기
 const addTodo = () => {
   const todo = document.querySelector(".todo-input").value;
   const newTodo = {
-    id: todolist.length + 1,
-    todo,
+    id: todosArray.length + 1,
+    content: todo,
     isDone: false,
   };
-  todolist.push(newTodo);
+  todosArray.push(newTodo);
+  console.log(todosArray);
 };
 
 // 투두리스트 완료하기
@@ -25,5 +60,10 @@ const deleteTodo = () => {
 };
 
 document.querySelector(".submit-btn").addEventListener("click", addTodo);
-document.querySelector("#completeBtn").addEventListener("click", completeTodo);
-document.querySelector("#deleteBtn").addEventListener("click", deleteTodo);
+// document.querySelector(".complete-Btn").addEventListener("click", completeTodo);
+// document.querySelector(".delete-Btn").addEventListener("click", deleteTodo);
+
+// 페이지 로딩 시에 실행되는 함수
+window.onload = () => {
+  paintTodos();
+};
